5b48f0664b01c8e583af0e64f5150d34c5502908
englandl <englandl@CompName>
2018-06-18 22:11:54 -0700

    Simple java code

---
+++ GoodbyeWorld/src/com/lee/Goodbye.java
@@ -0,0 +1,10 @@
+package com.lee;
+
+public class Goodbye {
+
+	public static void main(String[] args) {
+		System.out.println("This is not a test. This is an actual emergency. Goodbye!");
+
+	}
+
+}
